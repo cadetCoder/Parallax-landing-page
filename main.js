fa409de6c5b12d1c6fe2b159d9f847804db1e2b6
Your Name <darreltoledo@icloud.com>
2023-04-16 22:38:02 +0100

    added moved for hill1

--- main.js
+++ main.js
@@ -11,4 +11,5 @@ window.addEventListener('scroll', () => {
   leaf.style.left = value * 1.5 + 'px';
   hill5.style.left = value * 1.5 + 'px';
   hill4.style.left = value * -1.5 + 'px';
+  hill1.style.top = value * 1 + 'px';
 });
